06de0baed75c0f2f58dbc086f07609d6a8dd0e1d
Khom Outsourcify <khom@outsourcify.net>
2022-03-31 13:09:33 +0700

    [feat] Add entities

--- loop.js
+++ loop.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 for (let index = 0; index < 70; index++) {
-  fs.copyFile("source.txt", "destination.txt", (err) => {
+  fs.copyFile("src/Entity/Cat.php", `src/Entity/Cat${index + 1}.php`, (err) => {
     if (err) throw err;
     console.log("source.txt was copied to destination.txt");
   });
